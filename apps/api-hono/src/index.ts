9f4791a66f7f7c46f00c4898a84edba38ea47d95
kerem <keremtiryaki@gmail.com>
2024-07-25 21:41:57 +0300

    test lucia readBearerToken

--- apps/api-hono/src/index.ts
+++ apps/api-hono/src/index.ts
@@ -1,14 +1,30 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { luciaAuth } from './middlewares/lucia'
 
-const app = new Hono()
+type Variables = {
+  session: import("lucia").Session | null,
+  user: import("lucia").User | null
+}
+
+const app = new Hono<{ Variables: Variables }>()
+app.use(luciaAuth);
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text(`
+    
+    session: 
+    ${JSON.stringify(c.get('session'), null, 2)}
+
+
+    user: 
+    ${JSON.stringify(c.get('user'), null, 2)}
+    
+    `)
 })
 
 const port = 3000
-console.log(`Server is running on port ${port}`)
+console.log(`hono api is running on port ${port}`)
 
 serve({
   fetch: app.fetch,
