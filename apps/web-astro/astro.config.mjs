42ff760ccf5631856eadb407b6c9f15981e0dcfc
kerem <keremtiryaki@gmail.com>
2024-08-06 13:05:02 -0400

    remove all apps to clean Proof of concept codes

--- apps/web-astro/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    security: {
-        checkOrigin: true,
-    },
-    output: "server",
-});
